705f426c077f0a04fe14f2241e64eee6ffe2c318
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-02-06 15:01:43 +0000

    First commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const app = render(<App />);
+  const linkElement = app.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  const spreadApp = {...app}
+  const linkElement2 = spreadApp.getByText(/learn react/i);
+  expect(linkElement2).toBeInTheDocument();
 });
